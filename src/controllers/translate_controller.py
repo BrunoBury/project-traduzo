364ad86674e0147d520b0a36f6a051fec75a0a2f
Bruno <bruno_bury@hotmail.com>
2024-02-09 18:44:50 -0300

    req06

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from models.language_model import LanguageModel
+from deep_translator import GoogleTranslator
 
 language_controller = Blueprint("language_controller", __name__)
 
@@ -13,4 +14,24 @@ def index():
         translate_from="pt",
         translate_to="en",
         translated="What do you want to translate?",
+    )
+
+
+@language_controller.route("/", methods=["POST"])
+def post_index():
+    text_to_translate = request.form["text-to-translate"]
+    translate_from = request.form["translate-from"]
+    translate_to = request.form["translate-to"]
+
+    translator = GoogleTranslator(source=translate_from, target=translate_to)
+
+    translated = translator.translate(text_to_translate)
+
+    return render_template(
+        "index.html",
+        languages=LanguageModel.list_dicts(),
+        text_to_translate=text_to_translate,
+        translate_from=translate_from,
+        translate_to=translate_to,
+        translated=translated,
     )
